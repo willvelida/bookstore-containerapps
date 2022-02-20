a77c9323b3d054b54911ffa7815800578ac4c1d9
willvelida <willvelida@hotmail.co.uk>
2022-02-20 15:25:05 +1300

    Adding controllers to API

--- src/TodoApi/TodoApi/Controllers/TodoController.cs
+++ src/TodoApi/TodoApi/Controllers/TodoController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TodoApi.Common.Exceptions;
+using TodoApi.Common.Request;
+using TodoApi.Common.Response;
+using TodoApi.Services.Interfaces;
 
 namespace TodoApi.Controllers
 {
@@ -6,17 +10,124 @@ namespace TodoApi.Controllers
     [ApiController]
     public class TodoController : ControllerBase
     {
+        private readonly ITodoService _todoService;
         private readonly ILogger<TodoController> _logger;
 
-        public TodoController(ILogger<TodoController> logger)
+        public TodoController(ITodoService todoService, ILogger<TodoController> logger)
         {
+            _todoService = todoService;
             _logger = logger;
         }
 
         [HttpGet(Name = "GetTodoItem")]
-        public string Get()
+        public async Task<IActionResult> Get(string todoItemId)
         {
-            return "This is a Todo Item";
+            try
+            {
+                var todoItem = await _todoService.RetrieveTodoItem(todoItemId);
+
+                return new OkObjectResult(todoItem);
+            }
+            catch (NotFoundException nex)
+            {
+                _logger.LogError(nex.Message);
+                BaseResponse errorResponse = new BaseResponse();
+                errorResponse.SetErrorMessage(nex.Message);
+                return new CustomRequestObjectResult(errorResponse, StatusCodes.Status404NotFound);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                BaseResponse errorResponse = new BaseResponse();
+                errorResponse.SetErrorMessage(ex.Message);
+                return new CustomRequestObjectResult(errorResponse, StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet(Name = "GetAllTodoItems")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var todoItems = await _todoService.GetAllTodoItems();
+
+                return new OkObjectResult(todoItems);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                BaseResponse errorResponse = new BaseResponse();
+                errorResponse.SetErrorMessage(ex.Message);
+                return new CustomRequestObjectResult(errorResponse, StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost(Name = "CreateTodoItem")]
+        public async Task<IActionResult> Create([FromBody] TodoItemRequestDto todoItemRequestDto)
+        {
+            try
+            {
+                await _todoService.AddTodoItem(todoItemRequestDto);
+
+                return new CustomRequestObjectResult(null, StatusCodes.Status201Created);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                BaseResponse errorResponse = new BaseResponse();
+                errorResponse.SetErrorMessage(ex.Message);
+                return new CustomRequestObjectResult(errorResponse, StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost(Name = "UpdateTodoItem")]
+        public async Task<IActionResult> Post(string todoItemId, [FromBody] TodoItemRequestDto todoItemRequestDto)
+        {
+            try
+            {
+                await _todoService.UpdateTodoItem(todoItemId, todoItemRequestDto);
+
+                return new CustomRequestObjectResult(null, StatusCodes.Status204NoContent);
+            }
+            catch (NotFoundException nex)
+            {
+                _logger.LogError(nex.Message);
+                BaseResponse errorResponse = new BaseResponse();
+                errorResponse.SetErrorMessage(nex.Message);
+                return new CustomRequestObjectResult(errorResponse, StatusCodes.Status404NotFound);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                BaseResponse errorResponse = new BaseResponse();
+                errorResponse.SetErrorMessage(ex.Message);
+                return new CustomRequestObjectResult(errorResponse, StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpDelete(Name = "DeleteTodoItem")]
+        public async Task<IActionResult> Delete(string todoItemId)
+        {
+            try
+            {
+                await _todoService.DeleteTodoItem(todoItemId);
+
+                return new CustomRequestObjectResult(null, StatusCodes.Status204NoContent);
+            }
+            catch (NotFoundException nex)
+            {
+                _logger.LogError(nex.Message);
+                BaseResponse errorResponse = new BaseResponse();
+                errorResponse.SetErrorMessage(nex.Message);
+                return new CustomRequestObjectResult(errorResponse, StatusCodes.Status404NotFound);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                BaseResponse errorResponse = new BaseResponse();
+                errorResponse.SetErrorMessage(ex.Message);
+                return new CustomRequestObjectResult(errorResponse, StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
--- src/TodoApi/TodoApi/Program.cs
+++ src/TodoApi/TodoApi/Program.cs
@@ -1,6 +1,35 @@
+using Microsoft.Azure.Cosmos;
+using TodoApi.Common;
+using TodoApi.Repository;
+using TodoApi.Repository.Interfaces;
+using TodoApi.Services;
+using TodoApi.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddOptions<Settings>()
+    .Configure<IConfiguration>((settings, configuration) =>
+    {
+        configuration.GetSection("Settings").Bind(settings);
+    });
+builder.Services.AddSingleton(sp =>
+{
+    IConfiguration configuration = sp.GetService<IConfiguration>();
+    CosmosClientOptions cosmosClientOptions = new CosmosClientOptions
+    {
+        MaxRetryAttemptsOnRateLimitedRequests = 3,
+        MaxRetryWaitTimeOnRateLimitedRequests = TimeSpan.FromSeconds(60),
+        ApplicationRegion = "Canada Central",
+        SerializerOptions = new CosmosSerializationOptions
+        {
+            PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+        }
+    };
+    return new CosmosClient(configuration["CosmosDBConnectionString"], cosmosClientOptions);
+});
+builder.Services.AddTransient<ITodoRepository, TodoRepository>();
+builder.Services.AddTransient<ITodoService, TodoService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
